d242f2b4b3fd31504c4cdd3729d098786cece37b
shawntao9 <xiangyutao@yahoo.com>
2020-09-19 21:15:36 +0800

    gaussin naive bayes model for prediction

---
+++ term2/motion_planning/prediction/naive_bayes/classifier.h
@@ -0,0 +1,34 @@
+#ifndef CLASSIFIER_H
+#define CLASSIFIER_H
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <vector>
+#include <Eigen/Dense>
+
+using namespace std;
+
+class GNB {
+public:
+
+	vector<string> possible_labels = {"left","keep","right"};
+
+
+	/**
+  	* Constructor
+  	*/
+ 	GNB();
+
+	/**
+ 	* Destructor
+ 	*/
+ 	virtual ~GNB();
+
+ 	void train(vector<vector<double> > data, vector<string>  labels);
+
+  	string predict(vector<double>);
+
+};
+
+#endif
