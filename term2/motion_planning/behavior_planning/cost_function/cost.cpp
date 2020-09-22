5db15c174f611ddb76354f19d204afeb9c3d1645
shawntao9 <xiangyutao@yahoo.com>
2020-09-22 21:35:48 +0800

    add cost function for matching target speed

--- term2/motion_planning/behavior_planning/cost_function/cost.cpp
+++ term2/motion_planning/behavior_planning/cost_function/cost.cpp
@@ -19,3 +19,16 @@ float goal_distance_cost(int goal_lane, int intended_lane, int final_lane, float
     float cost = 1.0 - exp(-(delta_d / delta_s));
     return cost;
 }
+
+float inefficiency_cost(int target_speed, int intended_lane, int final_lane, vector<int> lane_speeds) {
+    /*
+    Cost becomes higher for trajectories with intended lane and final lane that have traffic slower than target_speed.
+    */
+    
+    //TODO: Replace cost = 0 with an appropriate cost function.
+    int intended_speed = lane_speeds[intended_lane];
+    int final_speed = lane_speeds[final_lane];
+    float cost = (2.0 * target_speed - intended_speed - final_speed) / target_speed;
+    
+    return cost;
+}
