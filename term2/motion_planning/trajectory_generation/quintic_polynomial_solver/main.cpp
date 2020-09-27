aa09d9d572a74fd0c551f4ad23d6d27383ace1c8
shawntao9 <xiangyutao@yahoo.com>
2020-09-27 23:42:04 +0800

    implement qp solver

--- term2/motion_planning/trajectory_generation/quintic_polynomial_solver/main.cpp
+++ term2/motion_planning/trajectory_generation/quintic_polynomial_solver/main.cpp
@@ -35,7 +35,26 @@ vector<double> JMT(vector< double> start, vector <double> end, double T)
     > JMT( [0, 10, 0], [10, 10, 0], 1)
     [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
     */
-    return {1,2,3,4,5,6};
+
+    MatrixXd A = MatrixXd(3,3);
+    A << T*T*T, T*T*T*T, T*T*T*T*T,
+         3*T*T, 4*T*T*T, 5*T*T*T*T,
+         6*T, 12*T*T, 20*T*T*T;
+
+    MatrixXd B = MatrixXd(3,1);
+    B << end[0] - (start[0] + start[1]*T + 0.5*start[2]*T*T),
+         end[1] - (start[1] + start[2]*T),
+         end[2] - start[2];
+
+    MatrixXd Ai = A.inverse();
+    MatrixXd C = Ai * B;
+
+    vector<double> result = {start[0], start[1], 0.5*start[2]};
+    for (int i = 0; i < C.size(); i++){
+        result.push_back(C.data()[i]);
+    }
+
+    return result;
     
 }
 
